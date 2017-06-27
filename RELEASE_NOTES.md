d8c5285833bfc82e63d1f82366ea9ea231795b1d
Kostas Stroggylos <stroggylos@gmail.com>
2017-06-27 12:00:25 +0300

    Added detailed release notes. Updated FAKE to 4.61.3

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,9 @@
 #### 1.2.1 June 17 2017 ####
 
-Support for Akka 1.2.1
+- Support for Akka 1.2.1
+- Targeting Grace 6.2.1
+- Updated target framework to 4.5.2
+
+#### 1.2.0 June 5 2017 ####
+
+Support for Akka 1.2.0
--- build.cmd
+++ build.cmd
@@ -24,8 +24,8 @@ pushd %~dp0
 
 src\.nuget\NuGet.exe update -self
 
-src\.nuget\NuGet.exe install FAKE -ConfigFile src\.nuget\Nuget.Config -OutputDirectory src\packages -ExcludeVersion -Version 4.16.1
-src\.nuget\NuGet.exe install xunit.runner.console -ConfigFile src\.nuget\Nuget.Config -OutputDirectory src\packages\FAKE -ExcludeVersion -Version 2.0.0
+src\.nuget\NuGet.exe install FAKE -ConfigFile src\.nuget\Nuget.Config -OutputDirectory src\packages -ExcludeVersion -Version 4.63.1
+src\.nuget\NuGet.exe install xunit.runner.console -ConfigFile src\.nuget\Nuget.Config -OutputDirectory src\packages\FAKE -ExcludeVersion -Version 2.2.0
 
 if not exist src\packages\SourceLink.Fake\tools\SourceLink.fsx (
   src\.nuget\nuget.exe install SourceLink.Fake -ConfigFile src\.nuget\Nuget.Config -OutputDirectory src\packages -ExcludeVersion
